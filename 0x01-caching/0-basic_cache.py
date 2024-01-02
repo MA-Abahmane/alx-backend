748754600e0a641c04ad1ba0f883e4723fe6844f
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-02 20:52:05 +0100

    Update base class

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
 
-"""
-A class BasicCache that inherits from
- BaseCaching and is a caching system
+""" A class BasicCache that inherits from
+    BaseCaching and is a caching system
 """
 
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ class BasicCache inherits from subclass BaseCaching """
+    """ class BasicCache inherits from subclass BaseCaching
+    """
 
     def put(self, key, item):
-        """ save key/value in cache """
+        """ save key/value in cache
+        """
         if key is None or item is None:
             return
 
         self.cache_data[key] = item
 
     def get(self, key):
-        """ retrieve value by key from cache """
+        """ retrieve value by key from cache
+        """
         if key is None or key not in self.cache_data:
             return
 

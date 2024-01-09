137e1c58510a5cb33fb23145658c923309579ce9
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-09 12:32:59 +0100

    Update task 0 II

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -14,9 +14,12 @@ app = Flask(__name__)
 
 # Define a route for the root URL ("/")
 @app.route('/')
-def index()  -> str:
+def index() -> str:
+    """ Flask app render
+    """
     # Render the '0-index.html' template with specified title and header
-    return render_template('0-index.html', title='Welcome to Holberton', header='Hello world')
+    return render_template('0-index.html', title='Welcome to Holberton',
+                            header='Hello world')
 
 # Run the Flask app if the script is executed directly
 if __name__ == '__main__':
